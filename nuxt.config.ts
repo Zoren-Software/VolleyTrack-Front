20cdf6d628d9286a482778b8d0d770fc9955b904
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-02-14 00:06:08 -0300

    feat: adicionado vuestic como ui front do nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: ['@vuestic/nuxt'],
+  vuestic: {
+    config: {
+      // Config here
+    },
+    css: ['typography', 'reset']
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Test</h1>
+    <p>Test component</p>
+  </div>
+</template>
