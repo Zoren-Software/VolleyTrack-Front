0a57a4acc9f533954ac07dda690910f88df8a9ac
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-02-26 23:14:42 -0300

    feat: feito layout do menu de login para a aplicação

---
+++ layouts/login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="row custom-width">
+    <div class="flex flex-col xs6 md6">
+      <div class="item">
+        <div class="row justify-center">
+          <div class="flex flex-col">
+            <div class="row">
+              <div class="flex items-center margin-custom-top">
+                <va-card 
+                  stripe 
+                  stripe-color="primary" 
+                  class="mx-5"
+                >
+                  <va-card-title>
+                    Login
+                  </va-card-title>
+                  <NuxtPage />
+                </va-card>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col md6 vuestic-hide-on-xs">
+      <va-carousel :arrows="false" :indicators="false" :items="items" stateful autoscroll :autoscrollInterval="30000" style="height: 100vh;"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        '/images/balls/ball-1.jpg',
+        '/images/balls/ball-2.jpg',
+        '/images/balls/ball-3.jpg',
+        '/images/balls/ball-4.jpg',
+        '/images/balls/ball-5.jpg',
+        '/images/balls/ball-6.jpg',
+        '/images/balls/ball-7.jpg',
+        '/images/balls/ball-8.jpg',
+        '/images/balls/ball-9.jpg',
+      ],
+    }
+  },
+}
+</script>
+
+<style scope>
+.margin-custom-top {
+  margin-top: 80%;
+}
+
+.custom-width {
+  min-width: 54rem !important;
+}
+</style>
---
+++ pages/login/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <va-form>
+    <div class="row justify-center px-3 pb-4">
+      <div class="flex flex-col">
+        <div class="item">
+          <va-input
+            v-model="inputValue1"
+            label="E-mail"
+            :rules="[value => (value && value.length > 0) || 'Este campo é obrigatório', value => verifyValueIsEmail() || 'Este campo deve ser um e-mail válido']"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="row justify-center px-3 pb-4">
+      <div class="flex flex-col">
+        <div class="item">
+          <va-input
+            v-model="inputValue2"
+            type="password"
+            label="Password"
+            :rules="[value => (value && value.length > 0) || 'Este campo é obrigatório']"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="row justify-center px-3 pb-3">
+      <va-button
+        :block="true"
+        color="primary"
+        @click="submit"
+      >
+        Login
+      </va-button>
+    </div>
+  </va-form>
+</template>
+
+<script>
+// This will work in both `<script setup>` and `<script>`
+definePageMeta({
+  layout: "login",
+});
+
+export default {
+  data () {
+    return {
+      inputValue1: '',
+      inputValue2: '',
+    }
+  },
+
+  methods: {
+    verifyValueIsEmail() {
+      if (this.inputValue1.includes('@')) {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
+}
+</script>
