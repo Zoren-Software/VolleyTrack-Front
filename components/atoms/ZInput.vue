77eabb81d2cdc9dc3bc4151d0c057230defa7353
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-05-25 21:32:30 -0300

    feat: criando componente de input com base no vuestic

---
+++ components/atoms/ZInput.vue
@@ -0,0 +1,50 @@
+<template>
+  <va-input
+    :id="id"
+    v-model="internalValue"
+    :label="label"
+    :rules="rules"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: String,
+      default: '',
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    rules: {
+      type: Array,
+      default: () => [],
+    },
+    id: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    internalValue: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value);
+      }
+    }
+  },
+  methods: {
+    verifyValueIsEmail() {
+      if (this.internalValue.includes('@')) {
+        return true
+      } else {
+        return false
+      }
+    },
+  },
+}
+</script>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -14,6 +14,17 @@
     <div class="row justify-center px-3 pb-4">
       <div class="flex flex-col">
         <div class="item">
+          <ZInput
+            v-model="email"
+            label="E-mail"
+            :rules="[value => (value && value.length > 0) || 'Este campo é obrigatório', value => verifyValueIsEmail() || 'Este campo deve ser um e-mail válido']"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="row justify-center px-3 pb-4">
+      <div class="flex flex-col">
+        <div class="item">
           <va-input
             v-model="password"
             type="password"
@@ -38,12 +49,17 @@
 <script>
 
 import LOGIN from '~/graphql/user/mutation/login.graphql'
+import ZInput from '~/components/atoms/ZInput'
 
 definePageMeta({
   layout: "login",
 });
 
 export default {
+  components: {
+    ZInput
+  },
+
   data () {
     return {
       email: 'admin@voleiclub.com',
@@ -51,7 +67,7 @@ export default {
     }
   },
 
-  methods: {
+  methods: {    
     verifyValueIsEmail() {
       if (this.email.includes('@')) {
         return true
