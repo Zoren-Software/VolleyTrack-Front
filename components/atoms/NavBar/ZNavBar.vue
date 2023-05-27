760dbb563f1dd56bf2d5d823f3079aa30ab321f2
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-05-27 18:12:31 -0300

    feat: criado componentização de NavBar e NavBarItem

---
+++ components/atoms/NavBar/ZNavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <va-navbar 
+    :color="color"
+    :class="class"
+    :style="style"
+    :shape="shape"
+  >
+    <slot name="left"></slot>
+    <slot name="default"></slot>
+    <slot name="right"></slot>
+  </va-navbar>
+</template>
+
+<script setup>
+
+import { defineComponent } from "vue";
+
+const props = defineComponent({
+  color: {
+    type: String,
+    default: "background-primary",
+  },
+  class: {
+    type: String,
+    default: "",
+  },
+  style: {
+    type: String,
+    default: "",
+  },
+  shape: {
+    type: String,
+    default: "rounded",
+  },
+});
+
+</script>
