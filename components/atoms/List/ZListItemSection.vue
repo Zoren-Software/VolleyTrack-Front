1ea0578dd472a38b6863492be354b09a3bba98b9
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-05-28 11:52:00 -0300

    feat: criado componentes de lista para padrão atômico

---
+++ components/atoms/List/ZListItemSection.vue
@@ -0,0 +1,26 @@
+<template>
+  <va-list-item-section
+    :avatar="avatar"
+    :icon="icon"
+  >
+    <slot name="default"/>
+  </va-list-item-section>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+      avatar: {
+        type: Boolean,
+        default: false,
+      },
+      icon: {
+        type: Boolean,
+        default: false,
+      },
+      
+    }
+});
+</script>
