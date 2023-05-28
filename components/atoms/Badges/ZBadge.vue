d1b83cda4c60bed8b842a3e789b82613e210e7ca
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-05-28 09:46:48 -0300

    feat: criado componente badge

---
+++ components/atoms/Badges/ZBadge.vue
@@ -0,0 +1,57 @@
+<template>
+  <va-badge
+    :class="class"
+    :text="text"
+    :color="color"
+    :placement="placement"
+    :dot="dot"
+    :overlap="overlap"
+    :transparent="transparent"
+    :visible-empty="visibleEmpty"
+
+  >
+    <slot />
+  </va-badge>
+</template>
+
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+      class: {
+        type: String,
+        default: '',
+      },
+      text: {
+        type: String,
+        default: '',
+      },
+      color: {
+        type: String,
+        default: 'danger',
+      },
+      placement: {
+        type: String,
+        default: 'top-end',
+      },
+      dot: {
+        type: Boolean,
+        default: false,
+      },
+      overlap: {
+        type: Boolean,
+        default: false,
+      },
+      transparent: {
+        type: Boolean,
+        default: false,
+      },
+      visibleEmpty: {
+        type: Boolean,
+        default: false,
+      },
+    }
+});
+</script>
