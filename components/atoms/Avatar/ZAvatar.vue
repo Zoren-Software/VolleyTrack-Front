28185b1fb77b6906414f5d2ebc3ced8b74406310
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-05-28 10:36:05 -0300

    feat: criado componente atômicos de avatar

---
+++ components/atoms/Avatar/ZAvatar.vue
@@ -0,0 +1,57 @@
+<template>
+  <va-avatar 
+    :class="class"
+    :src="src"
+    :fallback="fallback"
+    :icon="icon"
+    :color="color"
+    :size="size"
+    :square="square"
+    :loading="loading"
+  >
+    <slot name="default"/>
+  </va-avatar>
+</template>
+
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+      class: {
+        type: String,
+        default: '',
+      },
+      src: {
+        type: String,
+        default: '',
+      },
+      fallback: {
+        type: String,
+        default: '',
+      },
+      icon: {
+        type: String,
+        default: '',
+      },
+      color: {
+        type: String,
+        default: 'primary',
+      },
+      size: {
+        type: String,
+        default: 'medium',
+      },
+      square: {
+        type: Boolean,
+        default: false,
+      },
+      loading: {
+        type: Boolean,
+        default: false,
+      },
+      
+    }
+});
+</script>
