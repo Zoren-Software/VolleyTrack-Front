e331057706e97e7f59a585a79f2dfab5913da28a
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-06-21 21:16:22 -0300

    feat: adicionando opções de usabilidade para o componente de seleção de posições

--- components/molecules/Selects/ZSelectPosition.vue
+++ components/molecules/Selects/ZSelectPosition.vue
@@ -1,6 +1,27 @@
 <template>
   <div style="max-width: 300px">
-    <ZSelect v-model="value" v-bind="$attrs" :label="label" :options="items" />
+    <ZSelect
+      v-model="value"
+      v-bind="$attrs"
+      :label="label"
+      :options="items"
+      :loading="loading"
+      multiple
+      @click="getPositions"
+      @scrollBottom="loadMore"
+    >
+      <template #content="{ value }">
+        <va-chip
+          v-if="value.length >= 2"
+          v-for="chip in value.slice(0, 3)"
+          :key="chip.value"
+          class="mr-1 my-1 py-3"
+          size="small"
+        >
+          {{ chip.text }}
+        </va-chip>
+      </template>
+    </ZSelect>
   </div>
 </template>
 
@@ -23,55 +44,68 @@ export default {
       value: "",
       loading: false,
       items: [],
+      variablesGetPositions: {
+        page: 1,
+        perPage: 10,
+        filter: {
+          search: "",
+        },
+      },
     };
   },
-  mounted() {
-    this.getPositions();
-  },
+
   methods: {
     getPositions() {
       this.loading = true;
+      setTimeout(() => {
+        const query = gql`
+          ${POSITIONS}
+        `;
 
-      const query = gql`
-        ${POSITIONS}
-      `;
+        const {
+          result: { value },
+        } = useQuery(query, this.variablesGetPositions);
 
-      const {
-        result: { value },
-      } = useQuery(query, this.variablesGetPositions);
+        const { onResult } = useQuery(query, this.variablesGetPositions);
 
-      const { onResult } = useQuery(query, this.variablesGetPositions);
+        onResult((result) => {
+          this.handleResult(result.data);
+        });
 
-      onResult((result) => {
-        console.log(result);
-        if (result?.data?.positions?.data.length > 0) {
-          this.paginatorInfo = result.data.positions.paginatorInfo;
-          this.items = result.data.positions.data.map((item) => {
-            return {
-              text: item.name,
-              textBy: `Text by ${item.name}`,
-              value: item.id,
-              valueBy: item.name.toLowerCase(),
-            };
-          });
+        if (value) {
+          this.handleResult(value);
         }
-      });
+        this.loading = false;
+      }, 300);
+    },
 
-      if (value) {
-        console.log(value);
-        if (value?.positions?.data.length > 0) {
-          this.paginatorInfo = value.positions.paginatorInfo;
-          this.items = value.positions.data.map((item) => {
-            return {
-              text: item.name,
-              textBy: `Text by ${item.name}`,
-              value: item.id,
-              valueBy: item.name.toLowerCase(),
-            };
-          });
-        }
+    handleResult(result) {
+      if (result?.positions?.data.length > 0) {
+        this.paginatorInfo = result.positions.paginatorInfo;
+
+        const newItems = result.positions.data.map((item) => {
+          return {
+            text: item.name,
+            textBy: `Text by ${item.name}`,
+            value: item.id,
+            valueBy: item.name.toLowerCase(),
+          };
+        });
+
+        let allItems = [...this.items, ...newItems];
+
+        const uniqueItems = Array.from(
+          new Set(allItems.map((a) => a.value))
+        ).map((value) => {
+          return allItems.find((a) => a.value === value);
+        });
+
+        this.items = uniqueItems;
       }
-      this.loading = false;
+    },
+    loadMore() {
+      this.variablesGetPositions.page += 1;
+      this.getPositions();
     },
   },
 };
