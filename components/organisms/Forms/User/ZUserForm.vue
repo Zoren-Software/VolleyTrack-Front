6fce80001cc7aa9cb731b12893e63a528891dedb
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-07-03 21:42:31 -0300

    fix: ajustado valores do form

--- components/organisms/Forms/User/ZUserForm.vue
+++ components/organisms/Forms/User/ZUserForm.vue
@@ -1,34 +1,37 @@
 <template>
   <va-card class="my-3 mr-3">
-    <va-form ref="myForm" stateful class="flex flex-col gap-6 mb-2">
+    <va-form ref="myForm" class="flex flex-col gap-6 mb-2">
       <va-stepper v-model="step" :steps="steps" aria-label="lala">
         <template #step-content-0>
-          <ZTextInput name="name" label="Nome" id="name" class="mb-3" />
-          <ZEmailInput v-model="email" label="E-mail" id="email" class="mb-3" />
-
+          <ZTextInput
+            v-model="form.name"
+            name="name"
+            label="Nome"
+            id="name"
+            class="mb-3"
+          />
+          <ZEmailInput
+            v-model="form.email"
+            label="E-mail"
+            id="email"
+            class="mb-3"
+          />
           <ZPasswordInput
+            v-model="form.password"
             confirmPasswordInput
             name="password"
-            v-model="password"
             label="Senha"
             id="password"
             class="mb-3"
           />
         </template>
-        <template #step-content-1>
-          <ul>
-            <li>Fill out shipping information</li>
-            <li>Choose payment method</li>
-          </ul>
-        </template>
-        <template #step-content-2>
-          <ul>
-            <li>Fill out shipping information</li>
-            <li>Choose payment method</li>
-          </ul>
-        </template>
+        <!-- Outros steps ... -->
       </va-stepper>
     </va-form>
+    <pre>
+
+      {{ form }}
+    </pre>
   </va-card>
 </template>
 
@@ -48,7 +51,6 @@ export default {
   },
   data() {
     return {
-      password: "",
       formData,
       step: 0,
       steps: [
@@ -57,7 +59,18 @@ export default {
         { label: "Permissão" },
         { label: "Posição" },
       ],
+      form: {
+        name: "",
+        email: "",
+        password: "",
+        // Inicialize os dados de outros steps aqui...
+      },
     };
+  },
+  watch: {
+    step(val) {
+      console.log(val);
+    },
   },
 };
 </script>
