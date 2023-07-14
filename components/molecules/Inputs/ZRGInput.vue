c47f3f71e333ec17f094c14d1d1ee6206d3fcb2f
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-07-13 22:28:44 -0300

    fix: ajuste para valor de documento do jogador

--- components/molecules/Inputs/ZRGInput.vue
+++ components/molecules/Inputs/ZRGInput.vue
@@ -1,5 +1,12 @@
 <template>
-  <ZInput v-model="displayedValue" name="rg" label="RG" id="rg" class="mb-3" />
+  <ZInput
+    v-model="displayedValue"
+    name="rg"
+    label="RG"
+    id="rg"
+    class="mb-3"
+    maxlength="14"
+  />
 </template>
 
 <script>
