f873ae1f37560749c1791e903b5e2ac32390b5c3
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2024-02-01 16:25:58 -0300

    chore: ajuste edentação

--- components/organisms/List/Notification/ZListItemsNotification.vue
+++ components/organisms/List/Notification/ZListItemsNotification.vue
@@ -1,8 +1,6 @@
 <template>
   <va-list id="notification-dropdown">
-    <va-list-label>
-      Notificações
-    </va-list-label>
+    <va-list-label> Notificações </va-list-label>
     <ZListItemNotification
       v-for="action in actionsUser"
       :key="action.title"
@@ -12,41 +10,42 @@
   <div class="mt-3 mb-3">
     <va-divider />
   </div>
-  <va-button
-    to="/notifications"
-    size="small"
-    class="mr-6 mb-2 mr-5"
-  >
+  <va-button to="/notifications" size="small" class="mr-6 mb-2 mr-5">
     Ver todas as notificações
   </va-button>
-  <va-button
-    size="small"
-    class="mr-6 mb-2"
-  >
-    Limpar
-  </va-button>
+  <va-button size="small" class="mr-6 mb-2"> Limpar </va-button>
 </template>
 
 <script>
+import ZListItemNotification from "~/components/molecules/List/ZListItemNotification";
 
-import ZListItemNotification from '~/components/molecules/List/ZListItemNotification'
-
-export default{
+export default {
   components: {
     ZListItemNotification,
   },
-  data () {
+  data() {
     return {
       actionsUser: [
-        { title: 'Treino', description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', link: '/trainings' },
-        { title: 'Treino', description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', link: '/trainings' },
-        { title: 'Treino', description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', link: '/trainings' },
-      ]
-    }
+        {
+          title: "Treino",
+          description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+          link: "/trainings",
+        },
+        {
+          title: "Treino",
+          description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+          link: "/trainings",
+        },
+        {
+          title: "Treino",
+          description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+          link: "/trainings",
+        },
+      ],
+    };
   },
-}
+};
 </script>
-
-<style scoped>
-  /* seus estilos aqui */
-</style>
