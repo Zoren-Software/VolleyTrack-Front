5b2bc96d0af287575b21441b9f041f636bb102f5
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2024-02-10 21:03:39 -0300

    feat: fazendo os cards informativos para start do cliente

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,87 @@
 <template>
-  <div>
+  <div style="display: none">
     <h1>
       <!-- Mostrar aqui o token do usuario vindo de localstorage -->
       {{ token }}
       <pre>{{ JSON.stringify(user, null, 2) }}</pre>
-
-      <ZList> </ZList>
     </h1>
+  </div>
+  <div>
+    <h4 class="va-h4">Primeiros Passos no VolleyTrack</h4>
+    <p class="va-text-secondary py-3">
+      Bem-vindo ao VolleyTrack, a plataforma que transforma a gestão de equipes
+      de vôlei.
+      <br />
+      <span class="va-text-success">Inicie seu caminho para o sucesso</span>
+      organizando suas informações essenciais de maneira simples e eficiente.
+      <br />
+      Vamos começar?
+    </p>
+    <div class="row">
+      <div class="flex flex-col pr-3 md4">
+        <div class="item">
+          <ZCardRegisters
+            stripe
+            stripe-color="success"
+            title="Registro de Jogadores"
+            color="secondary"
+            textButton="Adicionar Jogadores"
+          >
+            <p>
+              Adicione detalhes dos jogadores para formar uma base sólida para
+              sua equipe.
+            </p>
+          </ZCardRegisters>
+        </div>
+      </div>
+      <div class="flex flex-col pr-3 md4">
+        <div class="item">
+          <ZCardRegisters
+            stripe
+            stripe-color="success"
+            title="Registro de Times"
+            color="secondary"
+            textButton="Adicionar Times"
+          >
+            <p>
+              Organize seus times de maneira eficaz, registrando cada detalhe
+              importante.
+            </p>
+          </ZCardRegisters>
+        </div>
+      </div>
+      <div class="flex flex-col pr-3 md4">
+        <div class="item">
+          <ZCardRegisters
+            stripe
+            stripe-color="success"
+            title="Registro de Treinos"
+            color="secondary"
+            textButton="Adicionar Treinos"
+          >
+            <p>
+              Planeje e registre treinos para maximizar o desempenho e a
+              preparação da equipe.
+            </p>
+          </ZCardRegisters>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import ZList from "~/components/atoms/List/ZList";
+import ZCard from "~/components/molecules/Cards/ZCard";
+import ZButton from "~/components/atoms/Buttons/ZButton";
+import ZCardRegisters from "~/components/organisms/Cards/ZCardRegisters.vue";
 
 export default {
   components: {
-    ZList,
+    ZCard,
+    ZButton,
+    ZCardRegisters,
   },
+
   data() {
     return {
       token: "",
