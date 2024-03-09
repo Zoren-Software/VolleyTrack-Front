beb9fc522075d1fed712f7b5745b669efb6a582e
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2024-03-09 14:55:18 -0300

    feat: em caso de perca de autenticação a aplicação automaticamente irá fazer com que o usuário seja deslogado do sistema e o obrigue e fazer login novamente

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -3,6 +3,7 @@ export default defineNuxtRouteMiddleware((to, from, next) => {
   const token = localStorage.getItem('apollo:default.token')
   
   if (to.path != '/login' && token == null) {
+      localStorage.removeItem('apollo:default.token') // Limpar o token
       return '/login'
   }
   
