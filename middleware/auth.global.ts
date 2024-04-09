511c03920a6261616fdfc110d04e9b1586ec9779
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2024-04-08 22:57:40 -0300

    fix(usuário): correção notificação e-mail usuário - VLT-153 (#24)
    * feat: ajustado sistema para abranger a tela de registro de senha dos usuários
    
    * feat: criado rota para fazer a criação da senha do registro inicial do usuário
    
    * feat: feito validação manualmente em tela para escrita de senha

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,13 +1,19 @@
-
 export default defineNuxtRouteMiddleware((to, from, next) => {
-  const token = localStorage.getItem('apollo:default.token')
+    const token = localStorage.getItem('apollo:default.token');
+    
+    // Verifica se a rota atual é /set-password e se há um email e um token na URL
+    const isSetPasswordRoute = to.path.startsWith('/set-password/') && to.params.email && to.params.token;
+
+    if (isSetPasswordRoute) {
+        return;
+    }
   
-  if (to.path != '/login' && token == null) {
-      localStorage.removeItem('apollo:default.token') // Limpar o token
-      return '/login'
-  }
-  
-  if (to.path == '/login' && token != null) {
-      return '/'
-  }
-})
+    if (to.path !== '/login' && token == null) {
+        localStorage.removeItem('apollo:default.token'); // Limpar o token
+        return '/login';
+    }
+    
+    if (to.path === '/login' && token != null) {
+        return '/';
+    }
+  });
---
+++ pages/set-password/[email]/[token].vue
@@ -0,0 +1,15 @@
+<template>
+  <ZSetPasswordForm />
+</template>
+
+<script setup>
+import ZSetPasswordForm from "~/components/organisms/Forms/SetPassword/ZSetPasswordForm";
+
+definePageMeta({
+  layout: "login",
+});
+
+useHead({
+  titleTemplate: "Set Password",
+});
+</script>
