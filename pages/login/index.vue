447a47ad6a7a86d244916cd7ffb20a2b0884e94b
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2025-03-20 22:28:11 -0300

    feat(login): adicionar logo e estilização ao formulário de login

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <ZLoginForm />
+  <div class="login-container">
+    <div class="logo-wrapper">
+      <img src="/images/logo/logo-volleytrack.png" alt="logo" class="logo" />
+    </div>
+    <ZLoginForm />
+  </div>
 </template>
 
 <script setup>
@@ -13,3 +18,44 @@ useHead({
   titleTemplate: "Login",
 });
 </script>
+
+<style scoped>
+.login-container {
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  text-align: center;
+}
+
+.logo-wrapper {
+  display: inline-block;
+  padding: 10px;
+  border-radius: 8px;
+  filter: blur(0);
+  position: relative;
+  z-index: 1;
+  margin-bottom: 20px;
+}
+
+.logo-wrapper::before {
+  content: "";
+  position: absolute;
+  top: -20px;
+  left: -20px;
+  right: -20px;
+  bottom: -20px;
+  background: linear-gradient(-45deg, #3f3431 50%, #dadada 50%);
+  filter: blur(44px);
+  z-index: -1;
+  border-radius: 16px;
+}
+
+.logo {
+  width: 245px;
+  height: auto;
+  display: block;
+  position: relative;
+  z-index: 2;
+}
+</style>
